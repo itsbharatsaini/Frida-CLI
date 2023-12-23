bcbee9091c282c581702de9939e747405e0dab4b
Alejandro Ubeto <142447560+alexUbeto@users.noreply.github.com>
2023-12-22 19:08:35 -0600

    feat(bot): :sparkles: Add success and error messages for config command
    Created an error message for when there is no configuration file and success messages for when it's created or updated.

--- fridacli/chatbot/predefined_phrases.py
+++ fridacli/chatbot/predefined_phrases.py
@@ -1,2 +1,17 @@
+from fridacli.interface.styles import add_styletags_to_string
+
 START_MESSAGE = "Hello! How may I help you?"
+
+# Error messages
 ERROR_STR = "This is not a coding related question, is there anything else I can assist you with?"
+ERROR_MISSING_CONFIGFILE = f"Missing configuration file. Execute {add_styletags_to_string('frida config', style='command')} to create it."
+
+# Success messages
+success_configfile_create = (
+    lambda path: f"Configuration file {add_styletags_to_string(path, style='path')} "
+    + f"{add_styletags_to_string(f'successfully created', style='success')}."
+)
+success_configfile_update = (
+    lambda path: f"Configuration file {add_styletags_to_string(path, style='path')} "
+    + f"{add_styletags_to_string(f'successfully updated', style='success')}."
+)
