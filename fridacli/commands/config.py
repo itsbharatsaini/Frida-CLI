c269b60f4cefbfd7d459b7d254b014c64d993975
Alejandro Ubeto <142447560+alexUbeto@users.noreply.github.com>
2023-12-27 18:15:57 -0600

    refactor(commands): :recycle: Refactor "config" command functions
    Implemented two new functions "read" and "write" config file; Implemented the recent "notification" function from "console" module; Added docstrings to each function.

--- fridacli/commands/config.py
+++ fridacli/commands/config.py
@@ -6,49 +6,62 @@ from fridacli.chatbot.predefined_phrases import (
     success_configfile_create,
     success_configfile_update,
 )
-from fridacli.config.env_vars import configfile_path
+from fridacli.config.env_vars import config_file_exists, config_file_path
 from fridacli.interface.console import Console
 from fridacli.interface.styles import add_styletags_to_string
 
 console = Console()
 
+# ========= frida config --list =========
+
+
+def read_config_file(path: str) -> str:
+    """Read the contents of a configuration file and returns it."""
+    with open(path, "r") as file_content:
+        configfile_content = file_content.read()
+    return configfile_content.rstrip("\n")
+
 
 def print_config_list() -> None:
     """Print the contents of the configuration file."""
-    if os.path.exists(configfile_path):
-        config_output = (
-            "Configuration file path: "
-            + f"{add_styletags_to_string(configfile_path,style='path')}\n"
-        )
-        with open(configfile_path, "r") as file_content:
-            config_output += file_content.read()
-        console.print(config_output, bottom=0)
+    if config_file_exists():
+        config_output = f"Configuration file path: {add_styletags_to_string(config_file_path,style='path')}"
+        config_output += "\n" + read_config_file(config_file_path)
+        console.print(config_output)
     else:
-        console.print(ERROR_MISSING_CONFIGFILE, alignment="center")
+        console.notification(ERROR_MISSING_CONFIGFILE)
+
+
+# ========= frida config =========
+
+
+def write_config_to_file(api_key: str, path: str) -> None:
+    """Write the configuration file with the API key."""
+    config_input = f"LLMOPS_FRIDACLI_API_KEY={api_key}"
+    command = f"echo {config_input} > {path}"
+    os.system(command)
 
 
 def configurate_api_keys() -> None:
     """Configure the API keys and write them to the configuration file."""
-    new_configfile = not os.path.exists(configfile_path)
+    new_configfile = not config_file_exists()
     if not new_configfile:
-        if not console.confirm(CONFIGFILE_OVERWRITE):
+        overwrite = console.confirm(CONFIGFILE_OVERWRITE)
+        if not overwrite:
             return
 
     api_key = console.input("Enter your Softtek SKD API key:", top=1)
-    config_input = f"LLMOPS_FRIDACLI_API_KEY={api_key}"
-    command = f"echo {config_input} > {configfile_path}"
-    os.system(command)
+    write_config_to_file(api_key, config_file_path)
 
     success_message = (
-        success_configfile_create(configfile_path)
+        success_configfile_create(config_file_path)
         if new_configfile
-        else success_configfile_update(configfile_path)
+        else success_configfile_update(config_file_path)
     )
-    console.print(
-        success_message,
-        style="system",
-        alignment="center",
-    )
+    console.notification(success_message)
+
+
+# ========= config command =========
 
 
 def exec_config(list_option: bool) -> None:
