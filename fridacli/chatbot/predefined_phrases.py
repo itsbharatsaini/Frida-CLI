17ebec4cb401772690c500ba54acd610b99acc03
Alejandro Ubeto <142447560+alexUbeto@users.noreply.github.com>
2023-12-27 18:08:17 -0600

    refactor(chatbot): :truck: Move string constants to predefined phrases
    Moved strings constants from env_vars to predefined_phrases module.

--- fridacli/chatbot/predefined_phrases.py
+++ fridacli/chatbot/predefined_phrases.py
@@ -1,6 +1,16 @@
+from fridacli.config.env_vars import BOT_NAME
 from fridacli.interface.styles import add_styletags_to_string
 
 START_MESSAGE = "Hello! How may I help you?"
+INTERRUPT_CHAT = f"Press {add_styletags_to_string('Ctrl+C','operation')} (keyboard interrupt) to exit the chat"
+WELCOME_SUBTITLE = add_styletags_to_string(
+    "This is your personal "
+    + add_styletags_to_string(f"{BOT_NAME} AI assistant", "bot")
+    + ", you can ask any coding related question directly in the command line or type "
+    + add_styletags_to_string("!help", "command")
+    + " to see the available commands.",
+    style="info",
+)
 CONFIGFILE_OVERWRITE = f"A {add_styletags_to_string('.fridacli',style='code')} configuration file already exists. Do you want to overwrite it?"
 
 # Error messages
--- fridacli/config/env_vars.py
+++ fridacli/config/env_vars.py
@@ -1,29 +1,21 @@
-import os
 from typing import Dict
-from fridacli.interface.styles import add_styletags_to_string
+import os
+
+
+config_file_path = f"{os.environ.get('HOME')}/.fridacli"
+BOT_NAME = "Frida"
+
+
+def config_file_exists() -> bool:
+    """Check if the configuration file already exists."""
+    return os.path.exists(config_file_path)
 
 
 def get_config_vars(path: str) -> Dict:
-    """"""
+    """Retrieve configuration variables from a given configuration file."""
     config_variables = {}
     with open(path, "r") as file:
         for line in file:
             key, value = line.strip().split("=")
             config_variables[key] = value
     return config_variables
-
-
-# Config env variables
-configfile_path = f"{os.environ.get('HOME')}/.fridacli"
-
-# Chatbot env variables
-BOT_NAME = "Frida"
-INTERRUPT_CHAT = f"Press {add_styletags_to_string('Ctrl+C','operation')} (keyboard interrupt) to exit the chat"
-WELCOME_SUBTITLE = add_styletags_to_string(
-    "This is your personal "
-    + add_styletags_to_string(f"{BOT_NAME} AI assistant", "bot")
-    + ", you can ask any coding related question directly in the command line or type "
-    + add_styletags_to_string("!help", "command")
-    + " to see the available commands.",
-    style="info",
-)
