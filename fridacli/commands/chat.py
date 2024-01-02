497e777f69d9f584fdbdbbc7d5ec0fb8ad48ae76
Alejandro Ubeto <142447560+alexUbeto@users.noreply.github.com>
2024-01-02 14:34:53 -0600

    feat(commands): :sparkles: Create basic chat interface
    Detected when user does not have a configuration file created; Created basic loop for the chat session interface; Created function to split the user input and get the command arguments out of it.

--- fridacli/commands/chat.py
+++ fridacli/commands/chat.py
@@ -1,12 +1,56 @@
-def load_path(path: str):
+from chatbot.predefined_phrases import ERROR_MISSING_CONFIGFILE
+from config.env_vars import config_file_exists, get_config_vars, get_username
+from interface.console import Console
+
+
+console = Console()
+
+
+def get_command_parts(command_string: str):
     """"""
-    print(f"path: {path}")
+    words = command_string.split(" ")
+    command = words[0]
+    command_args = words[1:] if len(words) > 1 else []
+    return command, command_args
+
+
+def chat_session(username: str) -> None:
+    """"""
+    chatting = True
+    console.start_panel()
+    while chatting:
+        try:
+            user_input = console.input(prefix=username, style="user")
+            is_empty = len(user_input.replace(" ", "")) == 0
+            is_command = user_input.startswith("!")
+
+            # If it's an empty input
+            if is_empty:
+                print()
+                continue
+
+            # If the input is a command
+            if is_command:
+                command, command_args = get_command_parts(user_input)
+
+            else:
+                console.print("Bot Response.", style="bot")
+
+        except KeyboardInterrupt:
+            print()
+            exit()
 
 
 def exec_chat(path: str | None, tokens: bool):
     """"""
-    print("Chat")
-    if path:
-        load_path(path)
-    if tokens:
-        print(f"tokens: {tokens}")
+    if not config_file_exists():
+        console.notification(ERROR_MISSING_CONFIGFILE)
+        return
+
+    console.print("Initializing...", style="process", bottom=0)
+
+    env_vars = get_config_vars()
+    api_key = env_vars["LLMOPS_FRIDACLI_API_KEY"]
+    username = get_username()
+
+    chat_session(username=username)
