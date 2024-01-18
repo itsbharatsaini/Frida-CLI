b465264ca017fc915f046f6856a86de85ba799e5
Alejandro Ubeto <142447560+alexUbeto@users.noreply.github.com>
2024-01-18 16:56:38 -0600

    feat(commands): :sparkles: Implement !help subcommand
    Created callback for the help command that prints a Rich table with the available subcommands information (from subcommands_info module).

--- fridacli/commands/subcommands/basic_cli.py
+++ fridacli/commands/subcommands/basic_cli.py
@@ -4,9 +4,11 @@ from fridacli.interface.system_console import SystemConsole
 from fridacli.interface.styles import (
     file_list_with_styles,
     format_to_columns,
+    format_to_table,
     print_padding,
 )
 from fridacli.config.env_vars import BOT_NAME
+from fridacli.commands.subcommands.subcommands_info import get_commands_df
 
 
 def ls_subcommand(*args, **kwargs) -> None:
@@ -30,9 +32,14 @@ def cd_subcommand(*args, **kwargs) -> None:
     print_padding()
 
 
-def help_subcommand(*args, **kwargs):
+def help_subcommand(*args, **kwargs) -> None:
     """"""
-    pass
+    subcommands_df = get_commands_df()
+    table_output = format_to_table(
+        subcommands_df, box=None, padding=True, expand=True, ratio=[1, 3, 2]
+    )
+    system_console: SystemConsole = kwargs["system_console"]
+    system_console.print(table_output, bottom=0, alignment="center")
 
 
 def exit_subcommand(*args, **kwargs) -> None:
