212f3c1975a1d5d5e32c9ee48d1896fa9fc6245d
Alejandro Ubeto <142447560+alexUbeto@users.noreply.github.com>
2024-01-18 16:51:49 -0600

    feat(ui): :lipstick: Create table formatter for Dataframes
    Created new function that receives a pandas Dataframe a returns ir as a Rich table.

--- fridacli/interface/styles.py
+++ fridacli/interface/styles.py
@@ -1,4 +1,7 @@
+from pandas import DataFrame
+from rich import box
 from rich.columns import Columns
+from rich.table import Table
 
 import os
 import re
@@ -42,6 +45,42 @@ def format_to_columns(iterables: Iterable):
     """"""
     columns_output = Columns(iterables, equal=True, expand=True)
     return columns_output
+
+
+def format_to_table(
+    data: DataFrame,
+    title: str = None,
+    show_header: bool = True,
+    box=box.ROUNDED,
+    expand: bool = False,
+    padding: bool = False,
+    border_color: str = "",
+    text_color: str = None,
+    ratio: list = [],
+) -> Table:
+    """"""
+    table = Table(
+        title=title,
+        show_header=show_header,
+        box=box,
+        expand=expand,
+        padding=(0, 3, 1 if padding else 0, 1),
+        style=border_color,
+    )
+
+    for iterator, column in enumerate(data.columns.to_list()):
+        column_ratio = ratio[iterator] if ratio else 0
+        table.add_column(column, ratio=column_ratio)
+
+    for i, row in data.iterrows():
+        row_data = list(map(str, row.tolist()))
+        if text_color:
+            row_data = [
+                add_styletags_to_string(element, text_color) for element in row_data
+            ]
+        table.add_row(*row_data)
+
+    return table
 
 
 def file_list_with_styles(file_list: list) -> list:
