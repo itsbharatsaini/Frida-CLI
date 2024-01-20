4c43617bd5240d655f933a3d376ff8114ecb0ace
Alejandro Ubeto <142447560+alexUbeto@users.noreply.github.com>
2024-01-19 18:19:29 -0600

    refactor(commands): :recycle: Refact subcommands with recent added path utilities
    Implemented the new module "path_utilities" functionalities into the subcommands package modules.

--- fridacli/commands/subcommands/basic_cli.py
+++ fridacli/commands/subcommands/basic_cli.py
@@ -1,25 +1,30 @@
-import os
-
+from fridacli.chatfiles.path_utilities import (
+    change_directory,
+    check_valid_dir,
+    get_current_dir,
+    get_home_path,
+    get_sorted_file_list,
+)
+from fridacli.config.env_vars import BOT_NAME
+from fridacli.commands.subcommands.subcommands_info import get_commands_df
+from fridacli.commands.subcommands.predefined_phrases import ERROR_PATH_DOES_NOT_EXIST
 from fridacli.interface.system_console import SystemConsole
 from fridacli.interface.styles import (
     file_list_with_styles,
     format_to_columns,
     format_to_table,
 )
-from fridacli.config.env_vars import BOT_NAME
-from fridacli.commands.subcommands.subcommands_info import get_commands_df
-from fridacli.commands.subcommands.predefined_phrases import ERROR_PATH_DOES_NOT_EXIST
 
 
 def ls_subcommand(*args, **kwargs) -> None:
     """"""
-    system_console: SystemConsole = kwargs["system_console"]
-    root = args[0] if args else os.getcwd()
-    valid_path = os.path.exists(root) and os.path.isdir(root)
+    system_console: SystemConsole = kwargs.get("system_console")
+    root = args[0] if args else get_current_dir()
+    valid_path = check_valid_dir(root)
     if not valid_path:
         system_console.notification(ERROR_PATH_DOES_NOT_EXIST(root))
         return
-    file_list = file_list_with_styles(sorted(os.listdir(root)))
+    file_list = file_list_with_styles(get_sorted_file_list(root))
     columns_output = format_to_columns(file_list)
     system_console.print(columns_output, bottom=0)
 
@@ -31,13 +36,13 @@ def pwd_subcommand(*args, **kwargs):
 
 def cd_subcommand(*args, **kwargs) -> None:
     """"""
-    system_console: SystemConsole = kwargs["system_console"]
-    new_directory = args[0] if args else os.path.expanduser("~")
-    valid_path = os.path.exists(new_directory) and os.path.isdir(new_directory)
+    system_console: SystemConsole = kwargs.get("system_console")
+    directory_to_move = args[0] if args else get_home_path()
+    valid_path = check_valid_dir(directory_to_move)
     if not valid_path:
-        system_console.notification(ERROR_PATH_DOES_NOT_EXIST(new_directory))
+        system_console.notification(ERROR_PATH_DOES_NOT_EXIST(directory_to_move))
         return
-    os.chdir(new_directory)
+    change_directory(directory_to_move)
 
 
 def help_subcommand(*args, **kwargs) -> None:
@@ -46,12 +51,12 @@ def help_subcommand(*args, **kwargs) -> None:
     table_output = format_to_table(
         subcommands_df, box=None, padding=True, expand=True, ratio=[1, 3, 1]
     )
-    system_console: SystemConsole = kwargs["system_console"]
+    system_console: SystemConsole = kwargs.get("system_console")
     system_console.print(table_output, top=0, bottom=0, alignment="center")
 
 
 def exit_subcommand(*args, **kwargs) -> None:
     """"""
-    system_console: SystemConsole = kwargs["system_console"]
+    system_console: SystemConsole = kwargs.get("system_console")
     system_console.notification(f"{BOT_NAME}CLI Chat session ended")
     exit()
--- fridacli/commands/subcommands/files_cli.py
+++ fridacli/commands/subcommands/files_cli.py
@@ -1,24 +1,40 @@
-import os
-
 from fridacli.chatfiles.file_manager import FileManager
+from fridacli.chatfiles.path_utilities import (
+    check_samepath,
+    change_directory,
+    get_relative_path,
+    get_current_dir,
+)
 from fridacli.interface.system_console import SystemConsole
 
 
 def open_subcommand(*args, **kwargs):
     """"""
-    system_console: SystemConsole = kwargs["system_console"]
-    file_manager: FileManager = kwargs["file_manager"]
+    system_console: SystemConsole = kwargs.get("system_console")
+    file_manager: FileManager = kwargs.get("file_manager")
 
+    if not args:
+        system_console.notification("Argument required")
+        return
+
+    path_to_open = args[0]
+    formatted_path = get_relative_path(path_to_open)
     active_folder = file_manager.get_folder_status()
-    if active_folder and system_console.confirm("Wanna close?"):
+    current_folder_active = check_samepath(get_current_dir(), path_to_open)
+
+    if active_folder and current_folder_active:
+        system_console.notification("Directory already open")
+        return
+
+    if active_folder and system_console.confirm("Wanna close current project?"):
         close_subcommand(file_manager=file_manager)
 
-    path = args[0] if args else os.getcwd()
-    file_manager.load_folder(path=path)
-    system_console.notification(path)
+    file_manager.load_folder(path=path_to_open)
+    change_directory(path_to_open)
+    system_console.notification(formatted_path)
 
 
 def close_subcommand(*args, **kwargs):
     """"""
-    file_manager: FileManager = kwargs["file_manager"]
+    file_manager: FileManager = kwargs.get("file_manager")
     file_manager.close_folder()
