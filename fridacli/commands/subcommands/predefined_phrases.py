5859f8e3f08eed95427c000a57022420de9c66be
Alejandro Ubeto <142447560+alexUbeto@users.noreply.github.com>
2024-01-19 20:17:33 -0600

    refactor(phrases): :recycle: Create "predefined_phrases" package
    Created new package that contains constant strings and lambda functions that return strings that will be used in the rest of the project packages.

--- fridacli/commands/subcommands/predefined_phrases.py
+++
@@ -1,8 +0,0 @@
-from fridacli.interface.styles import add_styletags_to_string
-
-
-ERROR_PATH_DOES_NOT_EXIST = (
-    lambda path: f"{add_styletags_to_string('Error:', 'error')} "
-    + f"{add_styletags_to_string(path, 'path')} "
-    + "directory does not exist"
-)
