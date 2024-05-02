5164c33420d57ace48156bbb8f9a0bfb49e66e0f
Lourdes Cajica <lourdes.cajica@softtek.com>
2024-05-02 17:58:24 -0600

    Format code

--- fridacli/commands/recipes/document_recipe.py
+++ fridacli/commands/recipes/document_recipe.py
@@ -6,7 +6,10 @@ from mdutils.mdutils import MdUtils
 from fridacli.logger import Logger
 from fridacli.frida_coder import FridaCoder
 from fridacli.file_manager import FileManager
-from .predefined_phrases import generate_document_for_funct_prompt, generate_full_document_prompt
+from .predefined_phrases import (
+    generate_document_for_funct_prompt,
+    generate_full_document_prompt,
+)
 from .documentation import extract_documentation_python, extract_documentation_csharp
 
 logger = Logger()
@@ -26,6 +29,7 @@ EXTENSIONS = {
     ],
 }
 
+
 def save_documentation(path, lines):
     if "docx" in path:
         doc = Document()
@@ -41,7 +45,7 @@ def save_documentation(path, lines):
             elif format == "text":
                 doc.add_paragraph(text)
             elif format == "bullet":
-                doc.add_paragraph(text, style='List Bullet')
+                doc.add_paragraph(text, style="List Bullet")
 
         doc.save(path)
     else:
@@ -72,6 +76,7 @@ def save_documentation(path, lines):
         logger.info(__name__, f"Creating doc in... {path}")
         mdFile.create_md_file()
 
+
 def extract_documentation(information, extension, one_function, funct_name):
     if extension == ".py":
         return extract_documentation_python(information, one_function, funct_name)
@@ -80,27 +85,33 @@ def extract_documentation(information, extension, one_function, funct_name):
     else:
         logger.info(__name__, f"Do not support {extension} extension by now.")
 
-def get_code_block(text, extension, one_function, funct_name = None):
+
+def get_code_block(text, extension, one_function, funct_name=None):
     try:
         code_pattern = re.compile(EXTENSIONS[extension][1], re.DOTALL)
         matches = code_pattern.findall(text)
-        
+
         if matches == []:
             logger.info(__name__, f"Did not match: {text}")
         else:
             information = {
-                    "language": matches[0][0],
-                    "code": matches[0][1],
-                    "description": matches[0][2].replace(EXTENSIONS[extension][2], "").replace("`", "")
-                }
-            
+                "language": matches[0][0],
+                "code": matches[0][1],
+                "description": matches[0][2]
+                .replace(EXTENSIONS[extension][2], "")
+                .replace("`", ""),
+            }
+
             logger.info(__name__, information)
-            lines = extract_documentation(information, extension, one_function, funct_name)
+            lines = extract_documentation(
+                information, extension, one_function, funct_name
+            )
             if lines is not None:
                 information["documentation"] = lines
             return information
     except Exception as e:
         logger.error(__name__, f"Error get code block from text using regex: {e}")
+
 
 def write_code_to_path(path: str, code: str):
     try:
@@ -108,7 +119,8 @@ def write_code_to_path(path: str, code: str):
             f.write(code)
     except Exception as e:
         pass
-        #logger.error(__name__, f"Error writing code to path: {e}")
+        # logger.error(__name__, f"Error writing code to path: {e}")
+
 
 def extract_functions(code):
     functions = []
@@ -131,7 +143,7 @@ def extract_functions(code):
             start_line = idx
             return_type = function_match.group(1)
             function_name = function_match.group(2)
-            #logger.info(__name__, f"line: {idx} Return: {function_match.group(1)} Name: {function_match.group(2)} Args: {function_match.group(3)}")
+            # logger.info(__name__, f"line: {idx} Return: {function_match.group(1)} Name: {function_match.group(2)} Args: {function_match.group(3)}")
             current_function = function_name
             current_function_lines = [line]
             brace_count = 1 if line.endswith("{") else 0
@@ -158,6 +170,7 @@ def extract_functions(code):
             current_function_lines = []
     return functions
 
+
 def document_file(
     formats,
     method,
@@ -174,10 +187,10 @@ def document_file(
             logger.info(__name__, f"working on {file}")
 
             full_path = file_manager.get_file_path(file)
-            doc_path = ''.join(full_path.split(file)[:-1])
-            
+            doc_path = "".join(full_path.split(file)[:-1])
+
             code = frida_coder.get_code_from_path(full_path)
-            num_lines = code.count('\n')
+            num_lines = code.count("\n")
 
             new_file = []
             new_doc = [("title", f"Documentation of the file '{file}'")]
@@ -187,19 +200,24 @@ def document_file(
             if method == "Quick":
 
                 if num_lines <= 300:
-                    logger.info(__name__, f"{extension} {file} with {num_lines} lines... 1")
+                    logger.info(
+                        __name__, f"{extension} {file} with {num_lines} lines... 1"
+                    )
                     prompt = generate_full_document_prompt(code, extension)
                     logger.info(__name__, f"{file} with {num_lines} lines... 2")
                     response = chatbot_agent.chat(prompt, True)
                     logger.info(__name__, f"{file} with {num_lines} lines... 3")
 
-                    while ("```" not in response or EXTENSIONS[extension][0] not in response) and i <= MAX_RETRIES:
+                    while (
+                        "```" not in response
+                        or EXTENSIONS[extension][0] not in response
+                    ) and i <= MAX_RETRIES:
                         logger.info(__name__, f"Retry # {i} for file {file}")
                         response = chatbot_agent.chat(prompt, True)
                         i += 1
                     logger.info(__name__, f"{file} with {num_lines} lines... 4")
                     logger.info(__name__, response)
-                    if ("```" in response and EXTENSIONS[extension][0] in response):
+                    if "```" in response and EXTENSIONS[extension][0] in response:
                         information = get_code_block(response, extension, False)
                         if information:
                             new_code = information["code"]
@@ -209,7 +227,7 @@ def document_file(
                     else:
                         logger.info(__name__, f"Check response: {response}")
                         new_file = code.splitlines()
-                    
+
                 else:
                     logger.info(__name__, f"Else... more than 300 lines")
                     code = code.splitlines()
@@ -218,19 +236,27 @@ def document_file(
                     start_line = functions[0]["start_line"]
                     end_line = functions[-1]["end_line"]
 
-                    new_file.extend(code[0:start_line - 1])
+                    new_file.extend(code[0 : start_line - 1])
 
                     for func in functions:
                         funct_name = func["name_of_function"]
-                        prompt = generate_document_for_funct_prompt(func["code"], extension)
+                        prompt = generate_document_for_funct_prompt(
+                            func["code"], extension
+                        )
                         response = chatbot_agent.chat(prompt, True)
 
-                        while ("```" not in response or EXTENSIONS[extension][0] not in response) and i <= MAX_RETRIES:
-                            logger.info(__name__, f"Retry # {i} for file {file} function {funct_name}")
+                        while (
+                            "```" not in response
+                            or EXTENSIONS[extension][0] not in response
+                        ) and i <= MAX_RETRIES:
+                            logger.info(
+                                __name__,
+                                f"Retry # {i} for file {file} function {funct_name}",
+                            )
                             response = chatbot_agent.chat(prompt, True)
                             i += 1
 
-                        if ("```" in response and EXTENSIONS[extension][0] in response):
+                        if "```" in response and EXTENSIONS[extension][0] in response:
                             information = get_code_block(response, True, funct_name)
                             if information:
                                 document_code = information["code"]
@@ -245,7 +271,6 @@ def document_file(
                             code = ("\n" + func["code"]).splitlines()
                             new_file.extend(code)
 
-
                     new_file.extend(code[end_line::])
                     new_code = "\n".join(new_file)
             else:
@@ -256,7 +281,11 @@ def document_file(
             if len(new_doc) > 1:
                 for doctype, selected in formats.items():
                     if selected:
-                        filename = (("readme_" + file).replace(extension, ".md")) if doctype == "md" else (("doc_" + file).replace(extension, ".docx"))
+                        filename = (
+                            (("readme_" + file).replace(extension, ".md"))
+                            if doctype == "md"
+                            else (("doc_" + file).replace(extension, ".docx"))
+                        )
                         save_documentation(doc_path + filename, new_doc)
             else:
                 pass
@@ -264,6 +293,7 @@ def document_file(
         logger.info(__name__, f"{e}")
     finally:
         thread_semaphore.release()
+
 
 async def exec_document(
     formats, method, chatbot_agent, file_manager: FileManager, frida_coder: FridaCoder
